1908261ed0f71cd2d40cb8f0c944464c56780705
GregMengelkamp <gmengelkamp@gmail.com>
2022-01-26 16:50:00 +0100

    test: verified behavior when modifying an existing exchange rate

--- js/test_money.js
+++ js/test_money.js
@@ -4,7 +4,7 @@ const Portfolio = require('./portfolio');
 const Bank = require('./bank');
 
 class MoneyTest {
-	constructor() {
+	setUp() {
 		this.bank = new Bank();
 		this.bank.addExchangeRate('EUR', 'USD', 1.2);
 		this.bank.addExchangeRate('USD', 'KRW', 1100);
@@ -79,6 +79,12 @@ class MoneyTest {
 		bank.addExchangeRate('EUR', 'USD', 1.2);
 		let tenEuros = new Money(10, 'EUR');
 		assert.deepStrictEqual(bank.convert(tenEuros, 'USD'), new Money(12, 'USD'));
+
+		this.bank.addExchangeRate('EUR', 'USD', 1.3);
+		assert.deepStrictEqual(
+			this.bank.convert(tenEuros, 'USD'),
+			new Money(13, 'USD'),
+		);
 	}
 
 	testConversionWithMissingExchangeRate() {
@@ -108,6 +114,7 @@ class MoneyTest {
 			let method = Reflect.get(this, m);
 			console.log('Running: %s()', m);
 			try {
+				this.setUp();
 				Reflect.apply(method, this, []);
 			} catch (e) {
 				if (e instanceof assert.AssertionError) {
